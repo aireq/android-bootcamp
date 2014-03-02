a4ce859ac35023e4823126df5cfbb3f972fcea6a
Eric Anastas <aireq303@gmai.com>
2014-03-02 13:06:01 -0800

    Now logging text to log file

--- Yamba/src/com/example/yamba/StatusActivity.java
+++ Yamba/src/com/example/yamba/StatusActivity.java
@@ -2,6 +2,8 @@ package com.example.yamba;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -47,6 +49,20 @@ public class StatusActivity extends Activity implements OnClickListener {
 	@Override
 	public void onClick(View arg0) {
 		// TODO Auto-generated method stub
+		
+		
+		String text = status_editText.getText().toString();
+		
+		
+		
+		Log.d("StatusActivity","Button Clicked! Test is "+text );
+		
+		
+		
+		
+	
+		
+		
 		
 	}
 
