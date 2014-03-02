c09bb274f9904d740b837201594384ac48699628
Eric Anastas <aireq303@gmai.com>
2014-03-02 12:48:07 -0800

    wired up button click onclick handler

--- Yamba/src/com/example/yamba/StatusActivity.java
+++ Yamba/src/com/example/yamba/StatusActivity.java
@@ -1,15 +1,40 @@
 package com.example.yamba;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class StatusActivity extends Activity {
+public class StatusActivity extends Activity implements OnClickListener {
 
+	Button postStatusButton;
+	EditText status_editText;
+	
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		//this line uses R to lookup activity_status.xml
+		//reads XML and creates java classes and set's properties
+		//after this line all widgets (buttons and controls) are in memory
 		setContentView(R.layout.activity_status);
+		
+		
+		//gets references to widgets
+		postStatusButton = (Button)findViewById(R.id.postStatusButton);
+		status_editText = (EditText)findViewById(R.id.editStatusText);
+		
+		
+		//sets this class as the click listener
+		postStatusButton.setOnClickListener(this);
+		
+		
+		
 	}
 
 	@Override
@@ -17,6 +42,12 @@ public class StatusActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.status, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
