31727191ca835308ac8be68b70fa713fc518fb61
Eric Anastas <aireq303@gmai.com>
2014-03-02 20:10:19 -0800

    Now posting to yamba.marakana.com

--- Yamba/src/com/example/yamba/StatusActivity.java
+++ Yamba/src/com/example/yamba/StatusActivity.java
@@ -1,6 +1,7 @@
 package com.example.yamba;
 
 import winterwell.jtwitter.Twitter;
+import winterwell.jtwitter.TwitterException;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,24 +12,22 @@ import android.widget.EditText;
 
 public class StatusActivity extends Activity {
 
+	static final String LOG_TAG = "StatusActivity";
 
 	EditText status_editText;
-	
-	
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		//this line uses R to lookup activity_status.xml
-		//reads XML and creates java classes and set's properties
-		//after this line all widgets (buttons and controls) are in memory
+
+		// this line uses R to lookup activity_status.xml
+		// reads XML and creates java classes and set's properties
+		// after this line all widgets (buttons and controls) are in memory
 		setContentView(R.layout.activity_status);
-		
-		
-		//gets references to widgets
-		status_editText = (EditText)findViewById(R.id.editStatusText);
-		
+
+		// gets references to widgets
+		status_editText = (EditText) findViewById(R.id.editStatusText);
+
 	}
 
 	@Override
@@ -38,24 +37,36 @@ public class StatusActivity extends Activity {
 		return true;
 	}
 
-
 	public void onClick(View arg0) {
-		
+
 		// TODO Auto-generated method stub
-		String statusText = status_editText.getText().toString();
-		Log.d("StatusActivity","Button Clicked! Test is "+statusText );
-		
-		
-		String user = "student";
-		String pass = "password";
-		
-		Twitter twitter = new Twitter(user,pass);
-		twitter.setAPIRootUrl("http://yamba.marakana.com/api");
-		
-		twitter.setStatus(statusText);
+		final String statusText = status_editText.getText().toString();
+		Log.d(LOG_TAG, "Posting new status: " + statusText);
 
-	
+		// This creates a new thread pointing to an anonymous method, and then
+		// starts it
+		Thread t = new Thread() {
+			public void run() {
+
+				try {
+					String user = "student";
+					String pass = "password";
+					Twitter twitter = new Twitter(user, pass);
+					twitter.setAPIRootUrl("http://yamba.marakana.com/api");
+					twitter.setStatus(statusText);
+					Log.d(LOG_TAG, "Successfuly posted status: " + statusText);
+
+				} catch (TwitterException e) {
+
+					Log.e(LOG_TAG, "Error posting status", e);
+
+					e.printStackTrace();
+
+				}
+			}
+		};
 		
+		t.start();
+
 	}
-
 }
