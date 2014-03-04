d7c625a99e6c785bdc1411890e0bb7b82136c206
Eric Anastas <aireq303@gmai.com>
2014-03-03 21:34:04 -0800

    Can now actualy stop service

--- Yamba/src/com/example/yamba/UpdaterService.java
+++ Yamba/src/com/example/yamba/UpdaterService.java
@@ -11,51 +11,71 @@ import android.os.IBinder;
 import android.util.Log;
 
 public class UpdaterService extends Service {
-	
+
 	static final String LOG_TAG = "UpdaterService";
+	static final int DELAY = 10000;
+	boolean running = false;
 
 	@Override
 	public IBinder onBind(Intent arg0) {
-		
+
 		Log.d(LOG_TAG, "onBind");
-		
+
 		return null;
 	}
-	
+
 	Twitter twitter;
 
 	@Override
 	public void onCreate() {
-	
+
 		super.onCreate();
-		
-		twitter = new Twitter("student","password");
+
+		twitter = new Twitter("student", "password");
 		twitter.setAPIRootUrl("http://yamba.marakana.com/api");
-		
+
 		Log.d(LOG_TAG, "onCreate");
 	}
-	
+
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
+
+		
+		running = true;
 		
 		new Thread() {
 			public void run() {
+
 				try {
-					List<Status> timeLine = twitter.getPublicTimeline();
-					for (Status status : timeLine) {
-						Log.d(LOG_TAG, String.format("%s: %s",
-								status.user.name, status.text));
+
+					while (running) {
+
+						List<Status> timeLine = twitter.getPublicTimeline();
+
+						for (Status status : timeLine) {
+							Log.d(LOG_TAG, String.format("%s: %s",
+									status.user.name, status.text));
+						}
+
+						
+						
+						Thread.sleep(DELAY);
 					}
-				} catch (TwitterException e) {
+				}
+
+				catch (InterruptedException e) {
+					Log.e(LOG_TAG, "Updater interrupted", e);
+				}
+
+				catch (TwitterException e) {
 					// TODO: handle exception
 				}
 			}
 		}.start();
-		
-		
+
 		Log.d(LOG_TAG, "onStartCommand");
 		return super.onStartCommand(intent, flags, startId);
-		
+
 	}
 
 	@Override
@@ -63,8 +83,9 @@ public class UpdaterService extends Service {
 
 		super.onDestroy();
 		
+		running = false;
+
 		Log.d(LOG_TAG, "onDestroy");
 	}
-
 
 }
