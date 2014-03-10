ae59516d214b3c571ca6594eb1d17c10d8c3a77a
Eric Anastas <aireq303@gmai.com>
2014-03-10 01:16:14 -0700

    Fixed date text in timeline activity using a ViewBinder

--- Yamba/src/com/example/yamba/TimelineActivity.java
+++ Yamba/src/com/example/yamba/TimelineActivity.java
@@ -1,13 +1,18 @@
 package com.example.yamba;
 
 import android.os.Bundle;
+import android.provider.VoicemailContract.Status;
 import android.app.Activity;
 import android.database.Cursor;
 
+import android.text.format.DateUtils;
 import android.util.Log;
+import android.view.View;
 
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.SimpleCursorAdapter.ViewBinder;
+import android.widget.TextView;
 
 public class TimelineActivity extends Activity {
 
@@ -31,7 +36,11 @@ public class TimelineActivity extends Activity {
 			list = (ListView) findViewById(R.id.item_list);
 			cursor = ((YambaApp) getApplication()).statusData.query();
 
+			
+			
 			adapter = new SimpleCursorAdapter(this, R.layout.row, cursor, FROM, TO);
+			
+			adapter.setViewBinder(VIEW_BINDER);
 
 			list.setAdapter(adapter);
 		} catch (Exception e) {
@@ -41,4 +50,41 @@ public class TimelineActivity extends Activity {
 		}
 
 	}
+	
+	
+	static final ViewBinder VIEW_BINDER = new ViewBinder()
+	{
+		
+		@Override
+		public boolean setViewValue(View view, Cursor cursor, int columnIdx)
+		{
+			//this is going to be called for every column
+			
+			//checks that the view is not equal to created at
+			if(view.getId() != R.id.text_created_at) return false;
+			else
+			{
+				
+				long time = cursor.getLong(cursor.getColumnIndex(StatusData.C_CREATED_AT));
+				CharSequence relTime = DateUtils.getRelativeTimeSpanString(time);
+				
+				TextView text = (TextView)view;
+				
+				text.setText(relTime);
+				
+				
+				return true;
+			}
+			
+			
+	
+		}
+		
+
+	};
+	
 }
+
+
+
+
